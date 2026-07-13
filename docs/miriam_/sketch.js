2100768c2b5377e20625b48c120a3b3501198c0d
Manuel Michel <mail@manuelmichel.de>
2026-07-13 12:48:06 +0200

    neuer sketch kaffeegedanken

--- docs/miriam_/sketch.js
+++ docs/miriam_/sketch.js
@@ -18,9 +18,7 @@ function setup() {
 function draw() {
 
   background("#f0e7d2");
-
   if (firstTouch) {
-
     // =========================
     // MINDESTENS 4 SICHTBAR
     // =========================
@@ -32,7 +30,6 @@ function draw() {
       }
     }
 
-
     // =========================
     // UPDATE ICONS
     // =========================
@@ -40,12 +37,10 @@ function draw() {
 
       let icon = icons[i];
 
-
       // -------------------------
       // FADE
       // -------------------------
       icon.alpha += (icon.targetAlpha - icon.alpha) * 0.05;
-
 
       // -------------------------
       // NORMAL MOVEMENT
@@ -54,7 +49,6 @@ function draw() {
         icon.x += icon.vx;
         icon.y += icon.vy;
       }
-
 
       // -------------------------
       // BORDER COLLISION
@@ -65,8 +59,6 @@ function draw() {
       if (icon.x > width - r) { icon.x = width - r; icon.vx *= -1; }
       if (icon.y < r) { icon.y = r; icon.vy *= -1; }
       if (icon.y > height - r) { icon.y = height - r; icon.vy *= -1; }
-
-
 
       // -------------------------
       // FOCUS MODE
@@ -87,344 +79,166 @@ function draw() {
         icon.size += (600 - icon.size) * 0.05;
 
       } else {
-
         icon.size += (350 - icon.size) * 0.05;
-
       }
-
-
 
       // -------------------------
       // OTHER ICONS FADE OUT
       // -------------------------
       if (focusIndex !== -1 && i !== focusIndex) {
         icon.targetAlpha = 40;
-
       } else if (focusIndex === -1) {
-
         icon.targetAlpha = 255;
-
       }
-
     }
   }
 
-
-
   // =========================
-  // REPULSION
+  // REPULSION (KEIN ÜBERLAPPEN)
   // =========================
-
   for (let i = 0; i < icons.length; i++) {
-
     for (let j = i + 1; j < icons.length; j++) {
-
 
       let a = icons[i];
       let b = icons[j];
 
-
       let dx = b.x - a.x;
       let dy = b.y - a.y;
-
 
       let distVal = sqrt(dx * dx + dy * dy);
       let minDist = (a.size + b.size) * 0.4;
 
-
       if (distVal < minDist && distVal > 0) {
 
-
         let overlap = (minDist - distVal) * 0.03;
-
 
         let nx = dx / distVal;
         let ny = dy / distVal;
 
-
         a.x -= nx * overlap;
         a.y -= ny * overlap;
 
-
         b.x += nx * overlap;
         b.y += ny * overlap;
-
       }
-
     }
-
   }
 
-
-
   // =========================
-  // DRAW ICONS
+  // DRAW
   // =========================
-
   for (let icon of icons) {
-
     push();
-
     tint(255, icon.alpha);
-
-    image(
-      icon.img,
-      icon.x,
-      icon.y,
-      icon.size,
-      icon.size
-    );
-
+    image(icon.img, icon.x, icon.y, icon.size, icon.size);
     pop();
-
   }
-
 }
-
-
 
 function checkInteraction(px, py) {
 
 
   if (focusIndex !== -1) return;
 
-
   for (let i = 0; i < icons.length; i++) {
-
 
     let icon = icons[i];
 
-
-    let d = dist(
-      px,
-      py,
-      icon.x,
-      icon.y
-    );
-
+    let d = dist(px, py, icon.x, icon.y);
 
     if (d < icon.size / 2) {
 
-
       focusIndex = i;
-
       icon.state = "focus";
 
-
-
-      // =========================
-      // AUDIO WECHSEL
-      // =========================
-
-      let currentSound = icon.sounds[icon.soundIndex];
-
-
-      console.log(currentSound);
+      console.log(icon.sound);
+      console.log(getAudioContext().state);
+      icon.sound.stop();
+      icon.sound.play();
       console.log(getAudioContext().state);
 
-
-      currentSound.stop();
-      currentSound.play();
-
-
-      console.log(getAudioContext().state);
-
-
-
-      // nächster Klick spielt anderes Audio
-
-      icon.soundIndex = (icon.soundIndex + 1) % 2;
-
-
-
-      currentSound.onended(() => {
-
+      icon.sound.onended(() => {
 
         icon.state = "idle";
-
         focusIndex = -1;
 
-
         for (let ic of icons) {
-
           ic.targetAlpha = 255;
-
         }
-
-
       });
-
-
 
       break;
-
     }
-
   }
-
 }
-
-
 
 function mousePressed() {
-
-
   if (firstTouch == false) {
-
-
     firstTouch = true;
-
-
 
     for (let i = 0; i < numIcons; i++) {
 
-
       icons.push({
-
-
         img: loadImage(`images/icon${i + 1}.png`),
-
-
-        sounds: [
-
-          loadSound(`audio/audio${i + 1}_1.mp3`),
-
-          loadSound(`audio/audio${i + 1}_2.mp3`)
-
-        ],
-
-
-        soundIndex: 0,
-
-
+        sound: loadSound(`audio/audio${i + 1}.mp3`),
+        // sound: new Pizzicato.Sound(`audio/audio${i + 1}.mp3`),
 
         x: random(width),
-
         y: random(height),
 
-
         vx: random(-0.8, 0.8),
-
         vy: random(-0.8, 0.8),
 
-
-
         baseSize: 350,
-
         size: 350,
 
-
         alpha: 255,
-
         targetAlpha: 255,
-
 
         state: "idle",
 
-
         wobbleT: random(1000)
-
-
       });
-
     }
-
-
   } else {
-
-
     checkInteraction(mouseX, mouseY);
-
-
   }
-
 }
-
-
-
 
 function touchStarted() {
-
-
   if (firstTouch == false) {
-
-
     firstTouch = true;
-
-
 
     for (let i = 0; i < numIcons; i++) {
 
-
       icons.push({
-
-
         img: loadImage(`images/icon${i + 1}.png`),
-
-
-        sounds: [
-
-          loadSound(`audio/audio${i + 1}_1.mp3`),
-
-          loadSound(`audio/audio${i + 1}_2.mp3`)
-
-        ],
-
-
-        soundIndex: 0,
-
-
+        sound: loadSound(`audio/audio${i + 1}.mp3`),
+        // sound: new Pizzicato.Sound(`audio/audio${i + 1}.mp3`),
 
         x: random(width),
-
         y: random(height),
 
-
         vx: random(-0.8, 0.8),
-
         vy: random(-0.8, 0.8),
 
-
-
         baseSize: 350,
-
         size: 350,
 
-
         alpha: 255,
-
         targetAlpha: 255,
-
 
         state: "idle",
 
-
         wobbleT: random(1000)
-
-
       });
-
     }
-
-
   } else {
-
-
     checkInteraction(mouseX, mouseY);
-
-
   }
-
-
   return false;
-
 }
 
-
-
-
 function windowResized() {
-
   resizeCanvas(windowWidth, windowHeight);
-
 }
